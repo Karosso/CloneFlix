55c5a24697e6d31aa864aabd4ee309698dba540a
Oscar <dias_oscar@hotmail.com>
2021-08-01 15:45:29 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+var carousel = document.getElementById('base')
+
+const images = [
+  "public/assets/img/mini1.jpg",
+  "public/assets/img/mini2.jpg",
+  "public/assets/img/mini3.jpg",
+  "public/assets/img/mini4.jpg",
+  "public/assets/img/mini5.jpg",
+  "public/assets/img/mini6jpg",
+  "public/assets/img/mini7.jpg",
+  "public/assets/img/mini8.jpg",
+  "public/assets/img/mini9.jpg",
+  "public/assets/img/mini10.jpg",
+]
